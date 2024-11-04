105bfc9b7c5b9cf76d5b8ee025bd9c5de3e7c967
saini24 <120770194+saini24@users.noreply.github.com>
2024-11-04 21:30:18 +0530

    Add files via upload
    This component is responsible for rendering the list of to-do items.
    Props:
    
    The todos array is passed as a prop from the App component.
    Rendering:
    
    The component uses the map function to iterate over the todos array and render each item as a list item (<li>).
    The key attribute is set to todo._id, which helps React identify which items have changed, are added, or are removed.

---
+++ components.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TodoList = ({ todos }) => (
+    <ul>
+        {todos.map(todo => (
+            <li key={todo._id}>{todo.text}</li>
+        ))}
+    </ul>
+);
+
+export default TodoList;
